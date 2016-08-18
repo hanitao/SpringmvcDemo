cf93767ee117883274d879d8bd553ec35c8fe7c5
hanitao <276248206@@qq.com>
2016-08-18 10:30:20 +0800

    测试项目

---
+++ src/main/java/com/cus/test/shop/App.java
@@ -0,0 +1,10 @@
+package com.cus.test.shop;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/cus/test/shop/Test.java
@@ -0,0 +1,27 @@
+package com.cus.test.shop;
+
+
+
+/**
+ * Title: Test
+ * Copyright: Copyright (C) 2002 - 2016 GuangDong Eshore Techonlogy Co. Ltd
+ * Company: 广东亿迅科技有限公司 IT互联网部
+ * @author: hanitao
+ * @version: 2.2
+ * @time:  2016年7月17日 下午4:53:42
+ */
+public class Test {
+
+	
+	
+	
+	
+	/**
+	 * @param var
+	 * @param var2
+	 * @return
+	 */
+	public String test(String var,String var2){
+		return null;
+	}
+}
